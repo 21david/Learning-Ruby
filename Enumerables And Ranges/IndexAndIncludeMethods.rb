a2bb3c209f1771f1caf921ef4327327e36a31f0c
David <>
2021-05-15 03:41:44 +0000

    Went over index() and include?() methods

---
+++ Enumerables And Ranges/IndexAndIncludeMethods.rb
@@ -0,0 +1,33 @@
+# topics: include?(), index()
+
+def line; puts '--------------------------'; end
+
+arr = ["SF", "NY", "LA", "TX", "OK", "FL"]
+
+# .index() gets the index of an element in an array
+puts arr.index("TX")  # 3
+
+# returns nothing for elements not in the array
+puts arr.index("WA") # doesn't return anything
+
+
+# .include?() returns a boolean, true/false whether its in the array or not
+puts arr.include?("TX")  # true
+puts arr.include?("WA")  # false
+
+line
+
+# .include?() works with strings too
+# convention for methods that return boolean to end in ?
+str = "hello astronauts"
+puts str.include?("e")  # true
+puts str.index("e")  # 1
+
+# it is case sensitive
+puts str.include?("E")  # false
+
+line
+
+# works with string inputs
+puts str.include?("astro")  # true
+puts str.index("astro")  # 6
