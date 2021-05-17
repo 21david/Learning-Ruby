539fcf02b4be884fd38f6ba77ca689536afd3308
David <david.espinosa.21@gmail.com>
2021-05-17 18:32:29 -0500

    Create ReverseWords.rb

---
+++ Enumerables And Ranges/Exercises/ReverseWords.rb
@@ -0,0 +1,15 @@
+# Write a method reverse_words that takes in a sentence string and returns the sentence with the order of the characters in each word reversed. Note that we need to reverse the order of characters in the words, do not reverse the order of words in the sentence.
+
+def reverse_words(sent)
+  arr = sent.split(" ")
+  sol = []
+  
+  arr.each do |word|
+    sol << word.reverse
+  end
+  
+  return sol.join(" ")
+end
+
+puts reverse_words('keep coding') # => 'peek gnidoc'
+puts reverse_words('simplicity is prerequisite for reliability') # => 'yticilpmis si etisiuqererp rof ytilibailer'
